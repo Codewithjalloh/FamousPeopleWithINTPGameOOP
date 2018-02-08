ebf1a1bc5fddaf082540b714a697200524e69411
David J <codewithjalloh@gmail.com>
2018-02-08 12:11:25 +0000

    getTheUserInput func added

---
+++ FamousPeopleWithINTPGameOOP/UserInput.swift
@@ -0,0 +1,20 @@
+//
+//  UserInput.swift
+//  FamousPeopleWithINTPGameOOP
+//
+//  Created by Jalloh on 08/02/2018.
+//  Copyright © 2018 Jalloh. All rights reserved.
+//
+
+import Foundation
+
+
+func getTheUserInput() -> String {
+    
+    let standardInput = FileHandle.standardInput
+    var input = NSString(data: standardInput.availableData, encoding: String.Encoding.utf8.rawValue)
+    
+    input = input!.trimmingCharacters(in: CharacterSet.newlines) as String
+    
+    return input! as String
+}

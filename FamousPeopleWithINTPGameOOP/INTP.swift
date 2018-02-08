b4c2299c3d6d002a0af5702d549b43bb9272964e
info@codewithjalloh.com <supremejalloh>
2018-02-08 12:05:46 +0000

    class INTP model added

---
+++ FamousPeopleWithINTPGameOOP/INTP.swift
@@ -0,0 +1,21 @@
+//
+//  INTP.swift
+//  FamousPeopleWithINTPGameOOP
+//
+//  Created by Jalloh on 08/02/2018.
+//  Copyright © 2018 Jalloh. All rights reserved.
+//
+
+import Foundation
+
+class INTP {
+    
+    let name: String
+    let description: String
+    
+    init(name: String,description: String ) {
+        self.name = name
+        self.description = description
+    }
+    
+}

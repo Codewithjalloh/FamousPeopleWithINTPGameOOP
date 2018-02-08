4fe48d446afcb9fa05a19ae9cdd052edf33486cf
info@codewithjalloh.com <supremejalloh>
2018-02-08 12:02:55 +0000

    Initial Commit

---
+++ FamousPeopleWithINTPGameOOP/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FamousPeopleWithINTPGameOOP
+//
+//  Created by Jalloh on 08/02/2018.
+//  Copyright © 2018 Jalloh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
